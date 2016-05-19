902314e7bd4338cf3229ac3d1c39342b0e43c272
richard-startsomething <richard@startsomething.io>
2016-05-18 19:51:31 -0700

    5-18-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+ 
+
+  get 'welcome/index'
 
   get 'pages/about'
 
   get 'pages/contact'
   
-  root 'pages/home'
+  root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
